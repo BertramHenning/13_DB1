82868b316b1cc8a1b0583bfc9ff0280806818a53
jesper Bang <jesper.xenia@gmail.com>
2017-03-28 11:03:29 +0200

    life fix

--- 13_DB1/src/connector01917/Constant.java
+++ 13_DB1/src/connector01917/Constant.java
@@ -6,10 +6,10 @@ package connector01917;
 public abstract class Constant
 {
 	public static final String
-		server					= "localhost",  // database-serveren
-		database				=  "labDB",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "root", // dit brugernavn = dit studienummer 
-		password				= ""; // dit password som du har valgt til din database
+		server					= "sql8.freemysqlhosting.net",  // database-serveren
+		database				=  "sql8166117",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "sql8166117", // dit brugernavn = dit studienummer 
+		password				= "Elxy6HhCla"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
